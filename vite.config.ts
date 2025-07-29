ed951b57f99e6d526ef044ff9d7e0ae220868785
Shadenessness <ov3rL0Rd.nso@gmail.com>
2025-07-29 09:21:51 +0200

    Fix Storybook build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { resolve } from 'node:path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
