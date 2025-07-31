0a4f456b79d62c3da87ea48c497249d612180304
Shadenessness <ov3rL0Rd.nso@gmail.com>
2025-07-31 05:51:48 +0200

    replace tailwind with scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -9,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), tailwindcss()],
+  plugins: [viteReact()],
   test: {
     globals: true,
     environment: 'jsdom',
